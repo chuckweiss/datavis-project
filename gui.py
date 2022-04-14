c9d6c216f65481971ddd9ef8a015fa785facd33d
David <47672067+Revert301@users.noreply.github.com>
2022-04-13 21:52:44 -0400

    Description buttons

--- gui.py
+++ gui.py
@@ -4,10 +4,10 @@ import tkinter as tk
 from tkinter import filedialog as fd
 import seaborn as sns
 import pandas as pd
-from matplotlib.widgets import Button
+import numpy as np
+from matplotlib.widgets import Button, SpanSelector
 import matplotlib.pyplot as plt
 import os
-
 from buildDataFrames import buildDataFrames
 from parseProjectData import parseProjectData
 
@@ -18,26 +18,55 @@ from matplotlib.backend_bases import key_press_handler
 from matplotlib.figure import Figure
 
 
-def select_file(figure, canvas):
+def select_file(figure, canvas, ax):
     dirname = fd.askdirectory()
     parsed_data = parseProjectData(dirname)
     folder_name = os.path.basename(dirname)
     dataframes = buildDataFrames(parsed_data)
 
     # filter out on wrist
-    plot_count = 0
-    ax = figure.subplots(3, 1)
-    for subject_id in dataframes:
-        df = dataframes[subject_id]
-        #df = df[df['On Wrist'] == True]   #idk if we should do this
-        df.set_index("Datetime (UTC)", inplace=True)
-        ax[plot_count].plot(df["Movement intensity"])
-        ax[plot_count].xaxis.set_major_locator(plt.MaxNLocator(4))
-        ax[plot_count].set_title(subject_id)
-        plot_count += 1
+    plot_count = 1
+    # for subject_id in dataframes:
+    #     df = dataframes[subject_id]
+    
+    df = dataframes["310"]
+    df.set_index("Datetime (UTC)", inplace=True)
 
+    ax[1].plot(df["Acc magnitude avg"], color='b')
+    ax[0].plot(df["Acc magnitude avg"], color='b')
+    ax[1].xaxis.set_major_locator(plt.MaxNLocator(4))
+    ax[1].set_title("Acc magnitude avg")
+
+    ax[2].plot(df["Eda avg"], color='k')
+    ax[0].plot(df["Eda avg"], color='k')
+    ax[2].xaxis.set_major_locator(plt.MaxNLocator(4))
+    ax[2].set_title("Eda avg")
+
+    ax[3].plot(df["Temp avg"], color='r')
+    ax[0].plot(df["Temp avg"], color='r')
+    ax[3].xaxis.set_major_locator(plt.MaxNLocator(4))
+    ax[3].set_title("Temp avg")
+
+    ax[4].plot(df["Movement intensity"], color='g')
+    ax[0].plot(df["Movement intensity"], color='g')
+    ax[4].xaxis.set_major_locator(plt.MaxNLocator(4))
+    ax[4].set_title("Movement intensity")
+
+
+    plot_count += 1
+        
+    ax[0].xaxis.set_major_locator(plt.MaxNLocator(4))
+    # span = SpanSelector(
+    #     ax[0],
+    #     onselect,
+    #     "horizontal",
+    #     useblit=True,
+    #     props=dict(alpha=0.5, facecolor="tab:blue"),
+    #     interactive=True,
+    #     drag_from_anywhere=True
+    # )
     root.geometry("1400x600")
-    figure.subplots_adjust(hspace=0.5)
+    figure.subplots_adjust(hspace=1.2)
     root.title(folder_name)
     
     b1.ax.patch.set_visible(True)
@@ -52,6 +81,24 @@ def select_file(figure, canvas):
     b3.label.set_visible(True)
     b3.ax.axis('on')
 
+    b4.ax.patch.set_visible(True)
+    b4.label.set_visible(True)
+    b4.ax.axis('on')
+
+
+# def onselect(xmin, xmax):
+#     indmin = ax[1].
+#     indmax = min(len(x) - 1, indmax)
+
+#     region_x = x[indmin:indmax]
+#     region_y = y[indmin:indmax]
+
+#     if len(region_x) >= 2:
+#         line2.set_data(region_x, region_y)
+#         ax2.set_xlim(region_x[0], region_x[-1])
+#         ax2.set_ylim(region_y.min(), region_y.max())
+#         fig.canvas.draw_idle()
+        
 
 def desc_popup():
     top=Toplevel(root)
@@ -79,30 +126,38 @@ filemenu = Menu(menubar, tearoff=0)
 
 figure = Figure(figsize=(20, 7), dpi=100)
 canvas = FigureCanvasTkAgg(figure, root)
+ax = figure.subplots(5, 1)
 canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=1)
 
-filemenu.add_command(label="Open", command=lambda: select_file(figure, canvas))
+filemenu.add_command(label="Open", command=lambda: select_file(figure, canvas, ax))
 
-axes1 = figure.add_axes([0.905, 0.16, 0.1, 0.075])
+axes1 = figure.add_axes([0.905, 0.113, 0.1, 0.075])
 b1 = plt.Button(axes1, label='Description', color="yellow")
 b1.on_clicked(description1)
 b1.ax.patch.set_visible(False)
 b1.label.set_visible(False)
 b1.ax.axis('off')
 
-axes2 = figure.add_axes([0.905, 0.45, 0.1, 0.075])
+axes2 = figure.add_axes([0.905, 0.285, 0.1, 0.075])
 b2 = plt.Button(axes2, label='Description', color="yellow")
 b2.on_clicked(description2)
 b2.ax.patch.set_visible(False)
 b2.label.set_visible(False)
 b2.ax.axis('off')
 
-axes3 = figure.add_axes([0.905, 0.75, 0.1, 0.075])
+axes3 = figure.add_axes([0.905, 0.457, 0.1, 0.075])
 b3 = plt.Button(axes3, label='Description', color="yellow")
 b3.on_clicked(description3)
 b3.ax.patch.set_visible(False)
 b3.label.set_visible(False)
 b3.ax.axis('off')
+
+axes4 = figure.add_axes([0.905, 0.632, 0.1, 0.075])
+b4 = plt.Button(axes4, label='Description', color="yellow")
+b4.on_clicked(description3)
+b4.ax.patch.set_visible(False)
+b4.label.set_visible(False)
+b4.ax.axis('off')
 
 canvas.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
 canvas.draw()
